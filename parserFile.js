e8a6f6153f895f0556e79a05be30cffd74fe2d51
brico <eliott.bricout@symbol-it.fr>
2019-02-28 19:44:34 +0100

    fix/ last line

--- parserFile.js
+++ parserFile.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const parseFile = (file) => {
     const data = fs.readFileSync(file, 'utf8').split('\r\n');
     return data
-        .slice(1, +data[0])
+        .slice(1, +data[0] + 1)
         .map((line, index) => {
             const cut = line.split(' ');
             return {id: index, vertical: cut[0] === 'V', tags: cut.slice(2)}
