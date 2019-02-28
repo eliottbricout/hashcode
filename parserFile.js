db363762ceced5e61093702d593522398aeefe3b
brico <eliott.bricout@symbol-it.fr>
2019-02-28 19:11:36 +0100

    parser

--- parserFile.js
+++ parserFile.js
@@ -3,6 +3,12 @@
 const fs = require('fs');
 
 const parseFile = (file) => {
-    return fs.readFileSync(file, 'utf8').split('\r\n');
+    return fs.readFileSync(file, 'utf8')
+        .split('\r\n')
+        .slice(1)
+        .map(line => {
+            const cut = line.split(' ');
+            return { vertical: cut[0] === 'V', tags: cut.slice(2) }
+        });
 };
 module.exports = {parseFile};
